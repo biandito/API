acb468a9c448312c0381e4d0792b93c7af616db9
biandito <bekatrivz@gmail.com>
2023-10-07 22:54:57 -0300

    update item transaction

--- models/item.js
+++ models/item.js
@@ -1,21 +1,14 @@
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
-  titulo: {
-    type: String,
-    required: true,
-  },
+const Item = mongoose.model('Item', {
+  titulo: String,
   autor: String,
   categoria: String,
   preco: Number,
   descricao: String,
-  status: {
-    type: String,
-    enum: ['ativo', 'inativo'],
-    default: 'ativo',
-  },
+  status: Boolean,
+  
 });
 
-const Item = mongoose.model('Item', itemSchema);
 
 module.exports = Item;
--- models/transaction.js
+++ models/transaction.js
@@ -1,28 +1,4 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  comprador: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  vendedor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  item: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Item',
-    required: true,
-  },
-  dataTransacao: {
-    type: Date,
-    default: Date.now,
-  },
-  valorTransacao: Number,
-});
-
-const Transaction = mongoose.model('Transaction', transactionSchema);
 
 module.exports = Transaction;
