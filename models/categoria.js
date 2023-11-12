c988856093a1b46483d6605515f246c087c7c7fe
biandito <bekatrivz@gmail.com>
2023-11-12 17:02:29 -0300

    upload

--- models/categoria.js
+++ models/categoria.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Categoria = mongoose.model('Categoria', {
   nome: String,
   descricao: String,
+  ativo:Boolean
 })
 
 module.exports = Categoria ;
--- models/transaction.js
+++ models/transaction.js
@@ -1,4 +1,22 @@
 import mongoose from "mongoose";
 
+const mongoose = require('mongoose')
+
+const Transacao = mongoose.model('Transacao', {
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cliente'
+    },
+    sellerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario'
+    },
+    itemId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item'
+    },
+    price: Number,
+    transactionDate: Date,
+})
 
 module.exports = Transaction;
